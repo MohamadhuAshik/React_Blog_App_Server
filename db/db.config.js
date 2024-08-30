5c4628e0d30d3d75f3f3fb33b6e9b7a7628b5670
MohamadhuAshik <mohamadashik268@gmail.com>
2024-08-30 12:06:36 +0530

    update Commit

--- db/db.config.js
+++ db/db.config.js
@@ -2,7 +2,7 @@ const dotenv = require("dotenv")
 const mongoose = require("mongoose")
 dotenv.config()
 
-const ConnectionString = process.env.DB_CONNECTION
+const ConnectionString = process.env.DB_CONNECTION  /* ||   "mongodb://localhost:27017/React_Blog" */
 const connectionToDataBase = async () => {
     try {
         await mongoose.connect(ConnectionString)
